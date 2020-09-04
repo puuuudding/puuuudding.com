3dbb22e23e8d12e5962da7d2a3bcb3b44a3402aa
Sean J. Bao <SeanB.9012@gmail.com>
2020-09-04 17:41:53 +0800

    Change to use mixinicon component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import IconCopyright from 'remixicon/icons/Business/copyright-line.svg';
+import IconHearts from 'remixicon/icons/Health/hearts-fill.svg';
+import IconGithub from 'remixicon/icons/Logos/github-fill.svg';
 import styles from './index.module.sass';
 
 function AppFooter() {
   return (
     <footer className={styles.appFooter}>
       <span className={styles.rightsText}>
-        puuuudding.com&nbsp;
-        <svg className={styles.copyrightIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm0 2c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm0 3c1.82 0 3.413.973 4.288 2.428l-1.714 1.029A3 3 0 1 0 12 15a2.998 2.998 0 0 0 2.573-1.456l1.715 1.028A4.999 4.999 0 0 1 7 12c0-2.76 2.24-5 5-5z" /></svg>
-        &nbsp;2020
+        puuuudding.com&nbsp;<IconCopyright className={styles.iconCopyright} />&nbsp;2020
       </span>
       <span className={styles.loveText}>
-        Built with&nbsp;
-        <svg className={styles.heartIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0H24V24H0z" /><path d="M16.5 3C19.538 3 22 5.5 22 9c0 7-7.5 11-10 12.5C9.5 20 2 16 2 9c0-3.5 2.5-6 5.5-6C9.36 3 11 4 12 5c1-1 2.64-2 4.5-2z" /></svg>
+        Build with&nbsp;
+        <IconHearts className={styles.iconHeart} />
         &nbsp;by Sean & Lillian
         <a href="https://github.com/puuuudding/puuuudding.com" target="_blank" rel="noreferrer">
-          <svg className={styles.githubIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z" /><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z" /></svg>
+          <IconGithub className={styles.iconGithub} />
         </a>
       </span>
     </footer>
